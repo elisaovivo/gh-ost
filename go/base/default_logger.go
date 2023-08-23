bb27e1416f407a5cc442d22f558838c7730b5c87
samuraichen <samuraichen@tencent.com>
2023-08-23 11:30:35 +0800

    add log buffer

--- go/base/default_logger.go
+++ go/base/default_logger.go
@@ -26,68 +26,80 @@ func (l *simpleLogger) Debug(args ...interface{}) {
 
 	s := log.Debug(args[0].(string), args[1:])
 	l.buf.WriteString(s)
+	l.buf.WriteString("\n")
 }
 
 func (l *simpleLogger) Debugf(format string, args ...interface{}) {
 	s := log.Debugf(format, args...)
 	l.buf.WriteString(s)
+	l.buf.WriteString("\n")
 }
 
 func (l *simpleLogger) Info(args ...interface{}) {
 	s := log.Info(args[0].(string), args[1:])
 	l.buf.WriteString(s)
+	l.buf.WriteString("\n")
 }
 
 func (l *simpleLogger) Infof(format string, args ...interface{}) {
 	s := log.Infof(format, args...)
 	l.buf.WriteString(s)
+	l.buf.WriteString("\n")
 }
 
 func (l *simpleLogger) Warning(args ...interface{}) error {
 	err := log.Warning(args[0].(string), args[1:])
 	l.buf.WriteString(err.Error())
+	l.buf.WriteString("\n")
 	return err
 }
 
 func (l *simpleLogger) Warningf(format string, args ...interface{}) error {
 	err := log.Warningf(format, args...)
 	l.buf.WriteString(err.Error())
+	l.buf.WriteString("\n")
 	return err
 }
 
 func (l *simpleLogger) Error(args ...interface{}) error {
 	err := log.Error(args[0].(string), args[1:])
 	l.buf.WriteString(err.Error())
+	l.buf.WriteString("\n")
 	return err
 }
 
 func (l *simpleLogger) Errorf(format string, args ...interface{}) error {
 	err := log.Errorf(format, args...)
 	l.buf.WriteString(err.Error())
+	l.buf.WriteString("\n")
 	return err
 }
 
 func (l *simpleLogger) Errore(err error) error {
 	e := log.Errore(err)
 	l.buf.WriteString(e.Error())
+	l.buf.WriteString("\n")
 	return e
 }
 
 func (l *simpleLogger) Fatal(args ...interface{}) error {
 	e := log.Fatal(args[0].(string), args[1:])
 	l.buf.WriteString(e.Error())
+	l.buf.WriteString("\n")
 	return e
 }
 
 func (l *simpleLogger) Fatalf(format string, args ...interface{}) error {
 	e := log.Fatalf(format, args...)
 	l.buf.WriteString(e.Error())
+	l.buf.WriteString("\n")
 	return e
 }
 
 func (l *simpleLogger) Fatale(err error) error {
 	e := log.Fatale(err)
 	l.buf.WriteString(e.Error())
+	l.buf.WriteString("\n")
 	return e
 }
 
